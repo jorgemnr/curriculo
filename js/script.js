528bb0ea87993281285c77b712e9fc038ab987af
Jorge Manuel das Neves Rodrigues <Jorgemnr@Gmail.Com>
2018-09-02 12:56:51 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Mostrar botão que volta para o topo
+window.onscroll = function () { fncBtnTopo(); };
+function fncBtnTopo() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btnTopo").style.display = "block";
+    } else {
+        document.getElementById("btnTopo").style.display = "none";
+    }
+}
+
+// Faz rolagem para o topo da pagina suavemente
+document.getElementById("btnTopo").onclick = function () { fncTopo() };
+function fncTopo() {
+    this.display = "none";
+    this.opacity = "0.5";
+    window.scroll({ top: 0, behavior: "smooth" });
+}
+
